84d74001dfd8e7070da987fc0b24373c0c907633
cka09191 <cka09191@gmail.com>
2024-03-16 13:35:01 +0900

    new file:   Makefile 	new file:   dev/fpga/verilog_spi/tb_counter_16bit.v 	new file:   dev/fpga/verilog_spi/tb_counter_16bit.vcd 	new file:   tb_counter_16bit.vcd 	new file:   test

---
+++ Makefile
@@ -0,0 +1,13 @@
+
+phony:
+	make help
+
+help:## Show this help.
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+wave:## Run timing test must specify uut
+	@echo "Running timing test"
+	@iverilog -o test ./dev/fpga/verilog_spi/tb_$(uut)  ./dev/fpga/verilog_spi/$(uut)
+	@echo "Running vvp"
+	@vvp test
+	@gtkwave `grep -oE '\$dumpfile\(\".+?\"\)' ./dev/fpga/verilog_spi/tb_$(uut) | sed 's/\$dumpfile(\"//g' | sed 's/")//g'`
---
+++ dev/fpga/verilog_spi/tb_counter_16bit.v
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+module tb_counter_16bit;
+    reg CLK;
+    wire [15:0] cnt;
+    reg start_clk = 0;
+    reg rst = 0;
+    wire sig;
+
+    counter_16bit uut (
+        .clk50Mhz(CLK), 
+        .rst(rst), 
+        .cnt(cnt),
+        .sig(sig)
+    );
+    always begin
+        #1;
+        if (start_clk == 1) begin
+            CLK <= ~CLK;
+        end
+    end
+    initial begin
+        $dumpfile("tb_counter_16bit.vcd");
+        $dumpvars(0, tb_counter_16bit);
+        $display("start");
+        CLK = 0;
+        start_clk = 0;
+        $display("0tick");
+        #1;
+        $display("1tick");
+        start_clk = 1;
+        #10;
+        $display("10tick");
+        #100;
+        rst = 1;
+        #100;
+        rst= 0;
+        #100;
+        start_clk = 0;
+        #100;
+        rst= 0;
+        #100;
+
+        rst= 1;
+        start_clk = 1;
+        #100;
+        start_clk = 0;
+        #100;
+
+        $finish;
+    end
+
+endmodule
