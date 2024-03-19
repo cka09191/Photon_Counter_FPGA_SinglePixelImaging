68a102be9f7390a844c7aef4dfaed9a37d82771c
DESKTOP-T9MDSRF\rlatk <rlatks0923@gmail.com>
2024-03-19 22:20:01 +0900

    modify counter.v add memory.v and symbol

--- dev/fpga/verilog_spi/counter.v
+++ dev/fpga/verilog_spi/counter.v
@@ -2,16 +2,27 @@ module counter_8bit(
   input wire clk50Mhz,
   input wire rst,
   input wire sig,
-  output reg [7:0] cnt=0
+  output reg [7:0] send_data=0,
+  output reg write_en=0	
   );
    
+reg [7:0] cnt = 3'hxxx;
+
+	
 always@(posedge clk50Mhz or posedge rst)
   
 begin
   
-  if(rst) cnt <= 1'b0; 
+  if(rst) begin
+  send_data <= cnt;
+  write_en <= 1;
+  cnt <= 1'b0; 
+  end 
 
-  else if(sig) cnt <= cnt + 1; 
+  else if(sig) begin 
+  write_en <= 0;
+  cnt <= cnt + 1; 
+  end 
 
   end
 
