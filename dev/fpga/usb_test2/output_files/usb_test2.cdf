4fc79e8cd2cc2505c409624604d8537a9c5c8b37
DESKTOP-T9MDSRF\rlatk <rlatks0923@gmail.com>
2024-04-05 19:33:12 +0900

    Create usb_test py_code and verilog_code Create main.py to control fpga_only_controller

---
+++ dev/fpga/usb_test2/output_files/usb_test2.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.1.0 Build 162 10/23/2013 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE22F17) Path("C:/Photon_Counter_FPGA/Photon_Counter_FPGA/dev/fpga/usb_test2/output_files/") File("usb_test2.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ dev/fpga/usb_test2/usb_test.v
@@ -0,0 +1,32 @@
+// SPI Byte Interface, top level module
+// Platform: Altera Cyclone IV using Quartus 21.1 (>=16.1)
+// Documentation: https://coertvonk.com/hw/math-talk/byte-exchange-with-a-fpga-as-slave-30818
+//
+// Demonstrates byte exchange with Arduino where FPGA is the SPI slave
+//   - The Arduino sends an alternating pattern of 0xAA and 0x55 to the FPGA.
+//   - On the FPGA, LED[0] will be on when it receives 0xAA.  Consequentially it will blink with 10% duty cycle.
+//   - The FPGA always returns 0x55, what is displayed on the serial port.
+//   
+// The protocol is specified at https://coertvonk.com/hw/math-talk/bytes-exchange-protocol-30814
+//
+// GNU GENERAL PUBLIC LICENSE Version 3, check the file LICENSE for more information
+// (c) Copyright 2015-2022, Coert Vonk
+// All rights reserved.  Use of copyright notice does not imply publication.
+// All text above must be included in any redistribution
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+// for SPI MODE 3
+module usb_test( input wire clk,  
+                 input wire [15:0] rx,
+					  output reg [0:0] LED = 0,
+					  output reg [15:0] tx = 0
+                  ); // PLL clock  locked
+								 
+ 
+	always @(posedge clk)
+	   if (rx==1'b1) begin LED[0] <= 1;  tx <= rx;  end
+		else if(rx==1'b0) begin LED[0] <= 0; tx <= rx; end
+			
+endmodule
