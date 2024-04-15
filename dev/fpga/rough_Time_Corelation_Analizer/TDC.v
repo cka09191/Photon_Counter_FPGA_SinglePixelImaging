7817bafd2848b31415b185251e71006a0ed15a40
Gyeongjun <cka09191@gmail.com>
2024-04-15 13:33:43 +0900

    modified:   dev/fpga/rough_Time_Corelation_Analizer/TDC.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/TDC.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/r_TCA.bdf

--- dev/fpga/rough_Time_Corelation_Analizer/TDC.v
+++ dev/fpga/rough_Time_Corelation_Analizer/TDC.v
@@ -2,7 +2,9 @@ module TDC(
 	input wire clk,
 	input wire pulse1,
 	input wire pulse2,
-	output reg [7:0] START_END_INTERVAL // [7] START , [6] END, [5:0] INTERVAL 
+	output reg START,
+	output reg END,
+	output reg [5:0] INTERVAL
 );
 
 endmodule
--- dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
+++ dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
@@ -1,12 +1,10 @@
 module plot_distributer(
 	input wire clk,
-	input wire [7:0] START_END_INTERVAL,
+	input wire START,
+	input wire END,
+	input wire [5:0] INTERVAL,
 	output reg [6:0] Addr
 );
 
-
-reg START = START_END_INTERVAL[7];
-reg END = START_END_INTERVAL[6];
-reg [5:0] INTERVAL = START_END_INTERVAL[5:0];
 
 endmodule
