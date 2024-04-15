4da1f83d3e889d477fd2641a093c9ca2bb13616c
DESKTOP-T9MDSRF\rlatk <rlatks0923@gmail.com>
2024-04-15 19:28:21 +0900

    modified:   dev/fpga/rough_Time_Corelation_Analizer/DataMemory.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/Memory.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/r_TCA.bdf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/r_TCA.qsf

--- dev/fpga/rough_Time_Corelation_Analizer/Memory.v
+++ dev/fpga/rough_Time_Corelation_Analizer/Memory.v
@@ -1,9 +1,9 @@
 module DataMemory (
     input wire clk,      // Clock signal
-	 input wire [6:0] addr,
+	 input wire [7:0] addr,
     output reg [Memory_WIDTH-1:0] data_out, // Output data
-	 input wire [1:0] Command,
-	 input wire Memory_add
+	 input wire [1:0] Command,				
+	 input wire Memory_add	
 );
 
 parameter Memory_WIDTH = 8192;
@@ -11,13 +11,21 @@ parameter Memory_WIDTH = 8192;
 reg [Memory_WIDTH-1:0] memory;
 reg [11:0] mem_addr;
 
+
 always@ (posedge clk) begin
-    mem_addr = {addr, 5'b0};
-	 memory[mem_addr +: 8] <= memory[mem_addr +: 8] + 1'b1; 
-	if(Command == 2'b01) begin
-		data_out <= {Memory_WIDTH{1'b0}};
+	
+	if(Memory_add) begin
+		mem_addr = {addr, 5'b0};
+		memory[mem_addr +: 8] <= memory[mem_addr +: 8] + 1'b1;
 	end
-end	
+
+	if(Command == 2'b01) begin
+		memory <= {Memory_WIDTH{1'b0}};
+	end 
+	
+	data_out <= memory;
+
+end
 
 
 endmodule
