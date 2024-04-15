f1feaf52a6c2ad4d2bbfaa4c4b8b56dc6ac5d34f
DESKTOP-T9MDSRF\rlatk <rlatks0923@gmail.com>
2024-04-15 16:45:45 +0900

    modified:   dev/fpga/rough_Time_Corelation_Analizer/Memory.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v

--- dev/fpga/rough_Time_Corelation_Analizer/Memory.v
+++ dev/fpga/rough_Time_Corelation_Analizer/Memory.v
@@ -2,7 +2,8 @@ module DataMemory (
     input wire clk,      // Clock signal
 	 input wire [6:0] addr,
     output reg [Memory_WIDTH-1:0] data_out, // Output data
-	 input wire [1:0] Command
+	 input wire [1:0] Command,
+	 input wire Memory_add
 );
 
 parameter Memory_WIDTH = 8192;
@@ -10,6 +11,7 @@ parameter Memory_WIDTH = 8192;
 reg [Memory_WIDTH-1:0] memory;
 
 always@ (posedge clk) begin
+	memory[32*addr:32*addr+7] <= memory[32*addr:32*addr+7] + 1'b1;
 	if(Command == 2'b01) begin
 		data_out <= {Memory_WIDTH{1'b0}};
 	end
--- dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
+++ dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
@@ -4,7 +4,8 @@ module plot_distributer(
 	input wire END,
 	input wire [5:0] INTERVAL,
 	input wire data_arrived,
-	output reg [6:0] Addr
+	output reg [6:0] Addr,
+	output reg Memory_add
 );
 
 parameter address_0 = 128;
