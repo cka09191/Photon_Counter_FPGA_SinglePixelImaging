b1d1541cfdf04d72fed692ab0fbb428b3c7646ba
Gyeongjun <cka09191@gmail.com>
2024-04-16 07:37:38 +0900

    modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/r_TCA.bdf

--- dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
+++ dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
@@ -12,30 +12,32 @@ module plot_distributer(
 parameter address_0 = 7'd128;
 reg [3:0] count = 0;
 
-/*
+reg add_internal = 0;
+
 always @(posedge clk) begin
-	if (Memory_add == 1'b1) begin
-		count <= count + 1;
+	if (add_internal == 1'b1) begin
 		if(count == 5) begin
 			Memory_add <= 1'b0;
 			count <= 0;
+			add_internal = 0;
+		end
+		else begin
+			count <= count + 1;
+			Memory_add <= 1'b1;
 		end
 	end
 end
-*/
+
 
 always @(posedge data_arrived) begin
 	if(START == 2'b00 && END == 2'b11 && INTERVAL == 7'b0000000) begin
 		Addr <= address_0;
-		Memory_add <= 1'b1;
 	end
 	else if(START == 2'b01 && END == 2'b10) begin
 		Addr <= address_0 + INTERVAL;
-		Memory_add <= 1'b1;
 	end
 	else if(START == 2'b10 && END == 2'b01) begin
 		Addr <= address_0 - INTERVAL;
-		Memory_add <= 1'b1;
 	end
 end
 endmodule
