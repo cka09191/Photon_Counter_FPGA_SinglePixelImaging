c6b6958eddd4b638a8c7fd77e69d109181b6f625
Gyeongjun <cka09191@gmail.com>
2024-04-17 10:19:00 +0900

    modified:   dev/fpga/rough_Time_Corelation_Analizer/Command.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/Command.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/DataMemory.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/Memory.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/PLLJ_PLLSPE_INFO.txt 	modified:   dev/fpga/rough_Time_Corelation_Analizer/SPI.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/TDC.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/TDC.v 	new file:   dev/fpga/rough_Time_Corelation_Analizer/output_files/pll.qip 	modified:   dev/fpga/rough_Time_Corelation_Analizer/output_files/r_TCA.sof 	modified:   dev/fpga/rough_Time_Corelation_Analizer/pll.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/pll.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/pll_bb.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.bsf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v 	modified:   dev/fpga/rough_Time_Corelation_Analizer/r_TCA.bdf 	modified:   dev/fpga/rough_Time_Corelation_Analizer/spi_short_module.bsf

--- dev/fpga/rough_Time_Corelation_Analizer/Command.v
+++ dev/fpga/rough_Time_Corelation_Analizer/Command.v
@@ -1,13 +1,13 @@
 module Command(
 	input wire clk,
-	input wire [8191:0] rx,
+	input wire [4095:0] rx,
 	output reg [1:0] Command,
 	input wire rxValid
 );
 
 always@ (posedge clk) begin
 	if(rxValid) begin
-	   if (rx == 8192'hFF) Command <= 2'b01;
+	   if (rx == 4096'hFF) Command <= 2'b01;
 	end else Command <= 2'b00;
 end
 
--- dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
+++ dev/fpga/rough_Time_Corelation_Analizer/plot_distributer.v
@@ -3,15 +3,15 @@ module plot_distributer(
 	input wire clk,
 	input wire [1:0] START,
 	input wire [1:0] END,
-	input wire [6:0] INTERVAL,
+	input wire [5:0] INTERVAL,
 	input wire data_arrived,
-	output reg [7:0] Addr = 0,
+	output reg [6:0] Addr = 0,
 	output reg Memory_add = 0
 );
 
 reg [2:0] data_arrived_r;  always @(posedge clk) data_arrived_r <= { data_arrived_r[1:0], data_arrived };
 wire data_arrived_rising  = ( data_arrived_r[2:1] == 2'b01 );
-parameter address_0 = 128;
+parameter address_0 = 64;
 reg [3:0] count = 0;
 
 reg add_internal = 0;
@@ -30,7 +30,7 @@ always @(posedge clk) begin
 	end
 	else count <= 0;
 	if(data_arrived_rising) begin
-		if(START == 2'b00 && END == 2'b11 && INTERVAL == 7'b0000000) begin
+		if(START == 2'b00 && END == 2'b11 && INTERVAL == 6'b000000) begin
 			Addr <= address_0;
 			Memory_add <= 1'b1;
 			count<=0;
